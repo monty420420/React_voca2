d5d424c18cb17e7fe3073d39a5ff3586d2832984
Muntaeyeon <monty420420@naver.com>
2021-12-07 22:44:37 +0900

    React_Route_comment

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import Day from './component/Day';
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import EmptyPage from './component/EmptyPage';
 function App() {
-  // <Route path="/day/:day">  :를 이용해서 day라는 변수로 해당하는값을 얻을수있음
+  // 컴포넌트 위치선정을 잘해야한다 EmptyPage를 위에놓을경우 EmptyPage가 기본이 된다.
+  // <Route path="/day/:day">  :를 이용해서 day라는 변수로 해당하는값을 얻을수있다
   return (
     <BrowserRouter>
     <div className="App">
