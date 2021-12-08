79752772725d6638b3de931ed3d1182f58777a05
Muntaeyeon <monty420420@naver.com>
2021-12-08 22:50:37 +0900

    React_button_checkBox

---
+++ src/component/Word.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+export default function Word({word}) {
+       const [isShow, setIsShow] = useState(false);
+       const [isDone, setIsDone] = useState(word.isDone);
+
+       function toggleShow(){
+           setIsShow(!isShow);
+       }
+
+       function toggleIsDone(){
+           setIsDone(!isDone);
+       }
+    return (
+
+        <tr className={isDone ? "off" : ''}>
+            <td>
+                <input type="checkbox" checked={isDone} onChange={toggleIsDone}
+                />
+            </td>
+            <td>
+                {word.eng}
+            </td>
+            <td>
+                {isShow && word.kor}
+            </td>
+            <td>
+                <button onClick={toggleShow}>뜻 {isShow ? "숨기기" : "보기"}</button>
+                <button className="btn_del">삭제</button>
+            </td>
+        </tr>
+    );
+}
